f6fc7ba3c9c8813d65b1294ace8ac14dfdf11391
araf95 <you@example.com>
2023-10-23 12:20:12 +0600

    arrow function

--- arrow.js
+++ arrow.js
@@ -1,5 +1,19 @@
-function name(n){
+var a = function name(n){
 	return n;
 }
 
-console.log(name('PORJOTOK.xyz'));
+console.log(a('PORJOTOK.xyz'));
+
+
+let fruit = (f) => { return f}
+console.log(fruit('Travel'));
+
+
+let destination = f => f;
+console.log('The world');
+
+let multiply = m => m*m*m;
+console.log(multiply(3));
+
+let add = (m,n) => m*n;
+console.log(add(3,2));
