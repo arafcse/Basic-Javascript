c580370346141ee2cef8784111aaf9d3968b6bb4
araf95 <you@example.com>
2023-10-23 11:57:42 +0600

    variable & arrow

---
+++ arrow.js
@@ -0,0 +1,5 @@
+function name(n){
+	return n;
+}
+
+console.log(name('PORJOTOK.xyz'));
---
+++ variable.js
@@ -0,0 +1,7 @@
+function foo(){
+	for (const i = 0; i < 4; i++) {
+		console.log('PORJOTOK.xyz');
+	}
+	console.log(i);
+}
+foo();
